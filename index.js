72e24697c86181d01744e2d88eb7d49476af6049
iamMHasan <abcimhasanbro@gmail.com>
2022-10-25 09:06:06 +0600

    api for courses added

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 const port = process.env.port || 5000;
+
+const courses = require('./courses.json')
+const courseDetails = require('./courseDetails.json')
+const premium = require('./premium.json')
 
 app.get('/', (req, res) => {
   res.send('skill hero server is running')
 })
+
+// api for courses catagory
+app.get('/courses', (req, res)=> {
+    res.send(courses)
+})
+
+
+// api for course details
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id 
+    const selectedCourse = courseDetails.find(c => c.id == id)
+    res.send(selectedCourse)
+})
+
+// api for checkout course
+app.get
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
